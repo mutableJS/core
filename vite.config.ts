b7002c89dcecd1b26db5c7fdfc09e957c9ce1770
Nikita Nitichevski <me@donnikitos.com>
2022-03-26 16:33:06 +0100

    added lib build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,14 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 
-export default defineConfig({});
+export default defineConfig({
+	build: {
+		lib: {
+			entry: path.resolve(__dirname, 'src/lib/index.ts'),
+			fileName: 'index',
+			formats: ['umd', 'cjs', 'es'],
+			name: 'mutation-test',
+		},
+		minify: false,
+	},
+});
