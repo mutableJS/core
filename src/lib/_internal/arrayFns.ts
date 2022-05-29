e63c4c3c0a1f324f42e320fbc7065f334aba24f9
Nikita Nitichevski <me@donnikitos.com>
2022-05-29 22:02:25 +0200

    updated array fn types + using set

--- src/lib/_internal/arrayFns.ts
+++ src/lib/_internal/arrayFns.ts
@@ -1,22 +1,33 @@
-const arrayFns = [
-	'concat',
-	'join',
-	'slice',
-	'every',
-	'some',
-	'map',
-	'filter',
-	'reduce',
-	'reduceRight',
-] as const;
+import { Mutable } from '../types';
 
-type ArrayFnNames = typeof arrayFns[number];
+type ArrayFnNames = keyof ArrayFns<any>;
+
+const arrayFns: Set<ArrayFnNames> = new Set(['every', 'some', 'sort', 'map', 'filter', 'reduce', 'reduceRight']);
 
 export function isArrayFn(input: any): input is ArrayFnNames {
-	return arrayFns.includes(input);
+	return arrayFns.has(input);
 }
 
-// TBD: Fix return types
-export type ArrayFns<Value> = Pick<Array<Value>, ArrayFnNames>;
-
-export default arrayFns;
+export type ArrayFns<Values> = {
+	every(
+		...params: Parameters<Array<Values>['every']>
+	): Mutable<ReturnType<Array<Values>['every']>>;
+	some(
+		...params: Parameters<Array<Values>['some']>
+	): Mutable<ReturnType<Array<Values>['some']>>;
+	sort(
+		...params: Parameters<Array<Values>['sort']>
+	): Mutable<ReturnType<Array<Values>['sort']>>;
+	map(
+		...params: Parameters<Array<Values>['map']>
+	): Mutable<ReturnType<Array<Values>['map']>>;
+	filter(
+		...params: Parameters<Array<Values>['filter']>
+	): Mutable<ReturnType<Array<Values>['filter']>>;
+	reduce(
+		...params: Parameters<Array<Values>['reduce']>
+	): Mutable<ReturnType<Array<Values>['reduce']>>;
+	reduceRight(
+		...params: Parameters<Array<Values>['reduceRight']>
+	): Mutable<ReturnType<Array<Values>['reduceRight']>>;
+};
